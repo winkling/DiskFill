aa71c6fa182f64ffea40a6b0fa538ee1e7005925
Kun Wang <mailwangkun@gmail.com>
2022-04-18 19:25:54 -0700

    Fix WriteAllBytesAsync might write empty files...

--- Program.cs
+++ Program.cs
@@ -1,46 +1,36 @@
-﻿namespace DiskFill
+﻿if (args.Length != 2)
 {
-    using System.IO;
+    Console.WriteLine("Usage: DiskFill [count] [round]");
+    return;
+}
 
-    internal class Program
+Console.WriteLine("Disk Fill started.");
+
+var count = int.Parse(args[0]);
+var round = int.Parse(args[1]);
+Console.WriteLine($"Run parameter: count={count}, round={round}.");
+
+var pathName = "Data";
+if (!Directory.Exists(pathName))
+{
+    Directory.CreateDirectory(pathName);
+}
+
+var bytes = new byte[1024 * 1024 * 1024];
+
+for (int r = 1; r <= round; r++)
+{
+    Console.WriteLine($"=== Round {r}");
+
+    var writeTaskList = new List<Task>();
+    for (int c = 1; c <= count; c++)
     {
-        static readonly string PathName = "Data";
-
-        static void Main(string[] args)
-        {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("Usage: DiskFill [count] [round]");
-                return;
-            }
-
-            Console.WriteLine("Disk Fill started.");
-
-            var count = int.Parse(args[0]);
-            var round = int.Parse(args[1]);
-            Console.WriteLine($"Run parameter: count={count}, round={round}.");
-
-            if (!Directory.Exists(PathName))
-            {
-                Directory.CreateDirectory(PathName);
-            }
-
-            var bytes = new byte[1024 * 1024 * 1024];
-
-            for (int r = 1; r <= round; r++)
-            {
-                Console.WriteLine($"=== Round {r}");
-
-                for (int c = 1; c <= count; c++)
-                {
-                    var file = Path.Combine("Data", $"{c}.bin");
-                    Console.Write($"Writing {file} ...");
-                    (new Random()).NextBytes(bytes);
-                    File.WriteAllBytesAsync(file, bytes);
-                    Console.WriteLine(" done.");
-                }
-            }
-
-        }
+        var file = Path.Combine(pathName, $"{c}.bin");
+        Console.Write($"Writing {file} ...");
+        (new Random()).NextBytes(bytes);
+        writeTaskList.Add(File.WriteAllBytesAsync(file, bytes));
+        Console.WriteLine(" done.");
     }
+
+    await Task.WhenAll(writeTaskList);
 }
