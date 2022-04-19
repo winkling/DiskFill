b2a7eb08d5eea7517d11d6382bfd527fa06695a3
Kun Wang <mailwangkun@gmail.com>
2022-04-18 23:29:27 -0700

    minor change

--- Program.cs
+++ Program.cs
@@ -22,15 +22,12 @@ for (int r = 1; r <= round; r++)
 {
     Console.WriteLine($"=== Round {r}");
 
-    var writeTaskList = new List<Task>();
     for (int c = 1; c <= count; c++)
     {
         var file = Path.Combine(pathName, $"{c}.bin");
         Console.Write($"Writing {file} ...");
         (new Random()).NextBytes(bytes);
-        writeTaskList.Add(File.WriteAllBytesAsync(file, bytes));
+        await File.WriteAllBytesAsync(file, bytes);
         Console.WriteLine(" done.");
     }
-
-    await Task.WhenAll(writeTaskList);
 }
